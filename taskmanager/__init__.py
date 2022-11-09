7722f55f3962424177a32caa0e3ac0ce6248bdcd
cwill83247 <breakthrumusic@hotmail.com>
2022-11-09 09:22:39 +0000

    Add: Adding Heroku Db ref in init

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,7 +6,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")       #std link to env.py
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")    #std link to env.py
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)   #this just adds sql to the url due to some sort of bug...
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)                                    #std SQLAlchment is whats imported above
 
