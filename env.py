d3d49952f8f48f2ee165614ad96de0364afddf50
cwill83247 <breakthrumusic@hotmail.com>
2022-11-09 10:45:11 +0000

    Add: removing env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")       #std
-os.environ.setdefault("PORT", "5000")                 #std
-os.environ.setdefault("SECRET_KEY", "any_secret_key")          #std but change the secret key
-os.environ.setdefault("DEBUG", "True")                          # std change to false for production
-os.environ.setdefault("DEVELOPMENT", "True")                    # stsd change for prpdcution 
-os.environ.setdefault("DB_URL", "postgresql:///taskmanager")   #std
